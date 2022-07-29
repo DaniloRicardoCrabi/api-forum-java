3e1e6f3707171a3aef628225e44083bd924e620c
Danilo <danilo.freitas@clubpetro.com>
2022-07-29 00:16:49 -0300

    habilitando o retorno com cache para a listagem de topicos

--- src/main/java/br/com/danilo/forum/controller/TopicosController.java
+++ src/main/java/br/com/danilo/forum/controller/TopicosController.java
@@ -9,6 +9,7 @@ import br.com.danilo.forum.repository.CursoRepository;
 import br.com.danilo.forum.repository.TopicoRepository;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -38,6 +39,7 @@ public class TopicosController {
 
 
     @GetMapping()
+    @Cacheable(value = "listaDeTopicos")
     public Page<TopicoDto> list(@RequestParam(required = false) String nomeCurso,
                               @PageableDefault(sort = "id",
                                                direction = Sort.Direction.DESC,
